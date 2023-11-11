c5d66a547396dee03d909e310a9b521b46b4a5d9
Daniel Ritz <d4r1tz@gmail.com>
2023-11-11 14:22:24 +0700

    Finished

---
+++ Data/UserStore.cs
@@ -0,0 +1,14 @@
+﻿using WebApplication1.Models.Dto;
+
+namespace WebApplication1.Data
+{
+    public static class UserStore
+    {
+        public static List<UserDTO> userList = new List<UserDTO>
+        {
+             new UserDTO{Id=1, Username="d4r1tz", Password="12345678"},
+             new UserDTO{Id=2, Username="qwerty", Password="12345678"}
+        };
+
+    }
+}
